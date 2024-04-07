646565b9bdfc4748c26c9ad0dea9c6aaf82587af
mehmetkr <mkaraoglu2002@yahoo.com>
2024-04-07 20:41:36 +0300

    Updated oracle server
    Updated oracle server

--- src/server/server.js
+++ src/server/server.js
@@ -14,12 +14,73 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
 
+let oracles = [];
+
+(async() => {
+  let accounts = await web3.eth.getAccounts();
+  try {
+      await flightSuretyData.methods.authorizeCaller(config.appAddress).send({from: accounts[0]});
+    } 
+    catch(e) {
+      console.log(e);
+    }
+
+  let fee = await flightSuretyApp.methods.REGISTRATION_FEE().call()
+
+  accounts.forEach( async(oracleAddress) => {
+    try {                                                                                         
+      await flightSuretyApp.methods.registerOracle().send({from: oracleAddress, value: fee, gas : 4712388, gasPrice: 100000000000 });
+      let indexesResult = await flightSuretyApp.methods.getMyIndexes().call({from: oracleAddress});
+      oracles.push({
+        address: oracleAddress,
+        indexes: indexesResult
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  });
+});
+
+console.log("Registering the Oracles.\n");
+setTimeout(() => {
+  oracles.forEach(oracle => {
+    console.log(`Oracle: address[${oracle.address}], indexes[${oracle.indexes}]`);
+  })
+
+  console.log("Watching for OracleRequest event for submitting the responses.")
+}, 25000)
+
+
 flightSuretyApp.events.OracleRequest({
     fromBlock: 0
   }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
-});
+    if (error) {
+      console.log(error)
+    } else {
+      let statusCode = Math.floor(Math.random() * 6) * 10;
+      let result = event.returnValues;
+      console.log(`OracleRequest: [${result.index}] for ${result.flight} ${result.timestamp}`);
+
+      oracles.forEach((oracle) => {
+      oracle.indexes.forEach((index) => {
+        flightSuretyApp.methods.submitOracleResponse(
+          index,
+          result.airline,
+          result.flight,
+          result.timestamp,
+          statusCode
+          ).send(
+          { from: oracle.address, gas: 4712388, gasPrice: 100000000000 }
+          ).then(res => {
+            console.log(`OracleResponse: address(${oracle.address}) index(${index}) accepted[${statusCode}]`)
+          }).catch(err => {
+            console.log(`OracleResponse: address(${oracle.address}) index(${index}) rejected[${statusCode}]`)
+          });
+        });
+      });
+    }
+  }
+);
 
 const app = express();
 app.get('/api', (req, res) => {
